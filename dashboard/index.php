3da9045e95d5e96ab1403abb81b27a1cc314b7a9
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-10-12 13:34:13 -0500

    Constantes movidas a archivo functions

---
+++ dashboard/index.php
@@ -0,0 +1,13 @@
+<?php
+    require_once("../config/functions.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo NOMBRE." " ?> Dashboard</title>
+</head>
+<body>
+    
+</body>
+</html>
