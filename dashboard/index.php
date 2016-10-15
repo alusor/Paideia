48292488a27e5fe76c25af5936de9bd783c670f6
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-10-15 11:21:40 -0500

    Se modifico el aspecto de las tarjetas

--- dashboard/index.php
+++ dashboard/index.php
@@ -6,9 +6,29 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo NOMBRE." " ?> Dashboard</title>
-    <link rel="stylesheet" href="<?php echo CSS?>style.css">
+    <!--<link rel="stylesheet" href="<?php echo CSS?>style.css">-->
+        <link rel="stylesheet" href="<?php echo CSS?>commonStyles.css">
+    <link rel="stylesheet" href="<?php echo CSS?>dashboardStyle.css">
+
 </head>
 <body>
-    hola
+    <aside id="sidebar">
+        <div class="header">
+            <span class="marca"> <?php echo NOMBRE?> </span>
+
+        </div>
+                    <img src="../img/perfil.jpg" />
+        <img >
+        <nav id="menu">
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Cursos</a></li>
+                <li><a href="">Mensajes</a></li>
+                <li><a href="">Configuración</a></li>
+                <li><a href="">Salir</a></li>
+            </ul>
+        </nav>
+        
+    </aside>
 </body>
 </html>
