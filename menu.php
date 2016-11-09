6f33c3430d2bb2a4486b4e83d38f5b2ba7898f5b
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-11-08 22:25:09 -0600

    Elementos innecesarios en el menu

--- menu.php
+++ menu.php
@@ -5,11 +5,10 @@
                         <?php echo NOMBRE;  ?>
                     </div>
                     <div class="menu">
-                        <a href="index.php">Inicio</a>
+                        <a href="index.php">Cursos</a>
                         <a href="vistaDetalle.php">Detalles</a>
                         <a href="vistaVideo.php">Video</a>
-                        <a href="">Menu</a>
-                        <a href="">Menu</a>
+
                     </div>
                 </div>
             </div>
