f88acb8d08084d0e922a7931c02e5c1f721dd28e
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-11-10 01:06:08 -0600

    Estructuración de control de rutas.

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
     include_once('funcitions.php');
     include_once('header.php');
-    
+    $controller = new pageController;
     
     if(isset($_GET['page'])){
-      $controller = new pageController;
+      
       $tempPage = $_GET['page'];
       $page;
       switch($tempPage){
         case 'inicio':
           $page = 'content.php';
         break;
+        default: $page = '404.php';
 
+        break;
       }
       $controller->loadContent($page);
+    }else{
+      $controller->loadContent('content.php');
     }
     
     include_once('footer.php');
