af0b6fb3d0067c3a7250ab723c349a7951fe3353
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-12-01 03:01:09 -0600

    testing conexion azure

--- config.php
+++ config.php
@@ -1,10 +1,34 @@
 <?php
 
+/*Cadena de conexion en Microsft Azure WebApp Mysql
+
+    
+*/
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+define("HOST",$connectstr_dbhost);
+    define("DBUSER",$connectstr_dbname);
+    define("DBPASS",$connectstr_dbpassword);
+    define("DBNAME","paideia");
+
     define('NOMBRE',"Paideia");
     define("DESCRIPCION",'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente facere cumque, saepe porro at repellat, praesentium nobis blanditiis! Magni quis, totam voluptatem inventore explicabo quidem aliquam incidunt accusamus necessitatibus temporibus.');
-    define("HOST","localhost");
+   /* define("HOST","localhost");
     define("DBUSER","root");
     define("DBPASS","");
-    define("DBNAME","paideia");
+    define("DBNAME","paideia");*/
 
 ?>
