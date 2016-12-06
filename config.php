72e15fffe8a4d2f214799968c6fd8ba9d21174ca
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-12-06 07:48:47 -0600

    constante nombre

--- config.php
+++ config.php
@@ -24,8 +24,8 @@ define("HOST",$connectstr_dbhost);
 
 
     //Conexcion local
-   /* define('NOMBRE',"Paideia");
-    define("DESCRIPCION",'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente facere cumque, saepe porro at repellat, praesentium nobis blanditiis! Magni quis, totam voluptatem inventore explicabo quidem aliquam incidunt accusamus necessitatibus temporibus.');
+   define('NOMBRE',"Paideia");
+   /* define("DESCRIPCION",'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente facere cumque, saepe porro at repellat, praesentium nobis blanditiis! Magni quis, totam voluptatem inventore explicabo quidem aliquam incidunt accusamus necessitatibus temporibus.');
     define("HOST","localhost");
     define("DBUSER","root");
     define("DBPASS","");
