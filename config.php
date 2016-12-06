0465e5cc98d9c8980fd76beb2a9accbcdf5813b0
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-12-06 07:41:34 -0600

    Cadena de conexion

--- config.php
+++ config.php
@@ -24,11 +24,11 @@ define("HOST",$connectstr_dbhost);
 
 
     //Conexcion local
-    define('NOMBRE',"Paideia");
+   /* define('NOMBRE',"Paideia");
     define("DESCRIPCION",'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente facere cumque, saepe porro at repellat, praesentium nobis blanditiis! Magni quis, totam voluptatem inventore explicabo quidem aliquam incidunt accusamus necessitatibus temporibus.');
     define("HOST","localhost");
     define("DBUSER","root");
     define("DBPASS","");
-    define("DBNAME","paideia");
+    define("DBNAME","paideia");*/
 
 ?>
