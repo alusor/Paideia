488f0c88918d3f0da97e2f50b3dcbf289025a69e
Eduardo Velez Santiago <eddvelez@outlook.com>
2016-12-08 04:12:05 -0600

    Muestra cursos correctamente, ir a inicio desde menu.

--- menu.php
+++ menu.php
@@ -4,15 +4,15 @@
  <nav>
             <div class="container">
                 <div class="navbar">
-                    <div class="marca">
-                        <?php echo NOMBRE;  ?>
+                    <div >
+                      <a class="marca" href="index.php?page=inicio">  <?php echo NOMBRE;  ?></a>
                     </div>
-                    <!--<div class="menu">
-                        <a href="index.php?page=inicio">Cursos</a>
-                        <a href="index.php?page=detalle">Detalles</a>
+                    <div class="menu">
+                        <a href="index.php?page=inicio">Inicio</a>
+                        <!--<a href="index.php?page=detalle">Detalles</a>
                         <a href="index.php?page=video">Video</a>
-
-                    </div>-->
+                        -->
+                    </div>
                 </div>
             </div>
         </nav>
